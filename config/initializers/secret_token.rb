189e6dce652592f355387e5811af8bcd62ec3de2
mohanraj23 <smmohanraj23@gmail.com>
2013-07-10 17:20:48 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thirstymorons::Application.config.secret_token = '94a6408c17f9615e4769dd97640ad4e6d44dc4f78d4f4707889426200c5c5c448c26fdc895d524e705ce98d66cfea8aa13f9ba8594b0bca59127637526a25451'
